a2d8bebe42a039c200dcba376a1d512f6ede8a8b
trees-software <treesbot@gmail.com>
2015-03-01 14:39:24 -0500

    updated

--- AutoLantern/Program.cs
+++ AutoLantern/Program.cs
@@ -4,7 +4,6 @@ using System;
 using System.Linq;
 using LeagueSharp;
 using LeagueSharp.Common;
-using LeagueSharp.Network.Packets;
 
 #endregion
 
@@ -12,10 +11,9 @@ namespace AutoLantern
 {
     internal class Program
     {
-        private const String Lantern = "ThreshLantern";
+        private const String LanternName = "ThreshLantern";
         private static Menu _menu;
         private static Obj_AI_Hero _player;
-        private static GameObject _threshLantern;
 
         private static void Main(string[] args)
         {
@@ -36,8 +34,6 @@ namespace AutoLantern
             _menu.AddToMainMenu();
 
             Game.OnGameUpdate += OnGameUpdate;
-            GameObject.OnCreate += OnMinionCreation;
-            GameObject.OnDelete += OnMinionDeletion;
 
             Game.PrintChat("AutoLantern by Trees loaded.");
             _player = ObjectManager.Player;
@@ -45,52 +41,28 @@ namespace AutoLantern
 
         private static void OnGameUpdate(EventArgs args)
         {
-            if (IsValid(_threshLantern) &&
-                ((IsLow() && _menu.Item("Auto").GetValue<bool>()) || (_menu.Item("Hotkey").GetValue<KeyBind>().Active)))
+            if ((!IsLow() || !_menu.Item("Auto").IsActive()) && (!_menu.Item("Hotkey").IsActive()))
             {
-                InteractObject(_threshLantern);
+                return;
             }
-        }
 
-        private static void OnMinionCreation(GameObject obj, EventArgs args)
-        {
-            if (obj != null && obj.IsValid && obj.IsAlly && obj.Name.Contains(Lantern))
+            var lantern =
+                ObjectHandler.Get<Obj_AI_Base>().Allies.FirstOrDefault(o => o.IsValid && o.Name.Equals(LanternName));
+            
+            if (lantern != null && lantern.IsValidTarget(500))
             {
-                _threshLantern = obj;
-            }
-        }
-
-        private static void OnMinionDeletion(GameObject obj, EventArgs args)
-        {
-            if (obj != null && obj.IsValid && obj.IsAlly && obj.Name.Contains(Lantern))
-            {
-                _threshLantern = null;
+                lantern.UseObject();
             }
         }
 
         private static bool IsLow()
         {
-            return _player.Health < _player.MaxHealth * _menu.Item("Low").GetValue<Slider>().Value / 100;
-        }
-
-        private static bool IsValid(GameObject lant)
-        {
-            return lant != null && lant.IsValid && _player.ServerPosition.Distance(lant.Position) <= 500;
+            return _player.HealthPercentage() <= _menu.Item("Low").GetValue<Slider>().Value;
         }
 
         private static bool ThreshInGame()
         {
             return ObjectManager.Get<Obj_AI_Hero>().Any(h => h.IsAlly && !h.IsMe && h.ChampionName == "Thresh");
-        }
-
-        private static void InteractObject(GameObject obj)
-        {
-            var p = new PKT_InteractReq
-            {
-                NetworkId = _player.NetworkId,
-                TargetNetworkId = obj.NetworkId
-            };
-            Game.SendPacket(p.Encode(), PacketChannel.C2S, PacketProtocolFlags.Reliable);
         }
     }
 }
